833a1508bb1b2c03cb53bc5bb80afb8e7ba96740
Dale Paul <dale-paul@Sisyphus.local>
2020-01-31 11:55:04 -0800

    match production by not binding local drives

--- run-neo4j.sh
+++ run-neo4j.sh
@@ -4,10 +4,6 @@ docker run \
      --detach \
      --publish=7474:7474 \
      --publish=7687:7687 \
-     --volume=$HOME/neo4j/data:/data \
-     --volume=$HOME/neo4j/logs:/logs \
-     --volume=$HOME/neo4j/conf:/conf \
-     --volume=$HOME/neo4j/plugins:/plugins \
-     --volume=$HOME/neo4j/import:/import \
+     --env NEO4J_dbms_security_auth__enabled=false \
      neo4j
 
